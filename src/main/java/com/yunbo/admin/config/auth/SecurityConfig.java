cd0cd4f09f566d20391474a85c7a447d3455af25
심윤보 <cscim918@gmail.com>
2022-02-08 16:57:40 +0900

    feat: 무중단 배포 스크립트 profile API 추가

--- src/main/java/com/yunbo/admin/config/auth/SecurityConfig.java
+++ src/main/java/com/yunbo/admin/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()// h2-console 화면을 사용하기 위해 해당 옵션들을 disable
                 .and()
                 .authorizeRequests()// URL별 권한 관리를 설정
-                .antMatchers("/", "/css/**","/images/**","/js/**","/h2-console/**").permitAll()// 전체 열람 권한
+                .antMatchers("/", "/css/**","/images/**","/js/**","/h2-console/**", "/profile").permitAll()// 전체 열람 권한
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())// 주소 가진 API는 USER만 가능
                 .anyRequest().authenticated()// 설정된 값 이외 나머지 URL들
                 // 권한 관리 대상을 지정하는 옵션. URL,HTTP 메소드별로 관리가 가능
@@ -29,5 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login()// 로그인 기능에 대한 여러 설정의 진입점
                 .userInfoEndpoint()// 로그인 성공 이후 사용자 정보를 가져올 때의 설정 담당
                 .userService(customOAuth2UserService);// 소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+
     }
 }
