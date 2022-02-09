3cd948907563b5eac3ab585f4f95ee8fb5accd81
심윤보 <cscim918@gmail.com>
2022-02-10 00:48:19 +0900

    fix: SecurityConfig profile URL 접속 허용

--- src/main/java/com/yunbo/admin/config/auth/SecurityConfig.java
+++ src/main/java/com/yunbo/admin/config/auth/SecurityConfig.java
@@ -17,10 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable()// h2-console 화면을 사용하기 위해 해당 옵션들을 disable
                 .and()
-                .authorizeRequests()// URL별 권한 관리를 설정
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()// 전체 열람 권한
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())// 주소 가진 API는 USER만 가능
-                .anyRequest().authenticated()// 설정된 값 이외 나머지 URL들
+                    .authorizeRequests()// URL별 권한 관리를 설정
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile/**").permitAll()// 전체 열람 권한
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())// 주소 가진 API는 USER만 가능
+                    .anyRequest().authenticated()// 설정된 값 이외 나머지 URL들
                 // 권한 관리 대상을 지정하는 옵션. URL,HTTP 메소드별로 관리가 가능
                 .and()
                     .logout()
