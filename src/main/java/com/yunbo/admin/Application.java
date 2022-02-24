606f2d7196f2276e7fc8a3510d70c85a7e65b2e4
심윤보 <cscim918@gmail.com>
2022-02-24 12:53:58 +0900

    chore: 사이트 제목 변경

--- src/main/java/com/yunbo/admin/Application.java
+++ src/main/java/com/yunbo/admin/Application.java
@@ -10,7 +10,7 @@ public class Application{
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
         //SpringApplication.run으로 인해 내장 WAS(Web Application Server)를 실행
-        //내장WAS-별도로 외부에 WAS를 두지 않고 애플리케이션을 실행할 때 내부에서 WAS를 실행하는 것
+        //내장WAS - 별도로 외부에 WAS를 두지 않고 애플리케이션을 실행할 때 내부에서 WAS를 실행하는 것
         //-> 이렇게 되면 항상 서버에 톰캣을 설치할 필요가 없게 되고, 스프링 부트로 만들어진 Jar 파일로 실행하면 됨
         //스프링 부트에서 내장 WAS 사용 권장(언제 어디서나 같은 환경에서 스프링부트를 배포할 수 있기 때문에)
     }
